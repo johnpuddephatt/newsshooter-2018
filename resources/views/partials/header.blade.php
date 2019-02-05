9a13c4e4f01e489f81c9b18f165af13799aa9e41
John Puddephatt <john@jdp.org.uk>
2019-02-05 00:12:05 +0000

    popover menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -20,6 +20,7 @@
       <a class="header--home-link" href="{{ home_url('/') }}">
         <img src="@asset('images/NS_Logo.svg')" width="250"/>
       </a>
+
       <div class="header--newsletter-link--container">
         <a href="https://newsshooter.us12.list-manage.com/subscribe/post?u=c882358b837abdc3bca261c95&id=c7e39b51db" class="header--button header--newsletter-link">
           <span class="icon">
@@ -33,39 +34,42 @@
     </div>
   </div>
 </header>
+
 <div class="primary-navigation--wrapper">
-  <div class="container">
-    @if (has_nav_menu('primary_navigation'))
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navigation primary-navigation']) !!}
-      <div class="menu-link--container">
-        <button class="menu-link">
-          <span class="icon">
-            <i class="fa fa-bars"></i>
-          </span>
-          <span class="sr-only menu-link--text">
-            Menu
-          </span>
-        </button>
-        <div class="menu-form--container">
-          <div class="container">
-            <button class="menu-close">
-              ×
-              <span class="sr-only menu-close--text">
-                Close
-              </span>
-            </button>
-            <img class="popover-logo" src="@asset('images/NS_Logo_Marque.svg')" width="450"/>
-            @if (has_nav_menu('footer_navigation_left'))
-              {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navigation popover-navigation']) !!}
-            @endif
-            @if (has_nav_menu('footer_navigation_left'))
-              {!! wp_nav_menu(['theme_location' => 'footer_navigation_right', 'menu_class' => 'navigtaion popover-navigation']) !!}
-            @endif
+  @if (is_home())
+    <div class="container">
+      @if (has_nav_menu('primary_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navigation primary-navigation']) !!}
+        <div class="menu-link--container">
+          <button class="menu-link">
+            <span class="icon">
+              <i class="fa fa-bars"></i>
+            </span>
+            <span class="sr-only menu-link--text">
+              Menu
+            </span>
+          </button>
+          <div class="menu-form--container">
+            <div class="container">
+              <button class="menu-close">
+                ×
+                <span class="sr-only menu-close--text">
+                  Close
+                </span>
+              </button>
+              <img class="popover-logo" src="@asset('images/NS_Logo_Marque.svg')" width="450"/>
+              @if (has_nav_menu('footer_navigation_left'))
+                {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navigation popover-navigation']) !!}
+              @endif
+              @if (has_nav_menu('footer_navigation_left'))
+                {!! wp_nav_menu(['theme_location' => 'footer_navigation_right', 'menu_class' => 'navigtaion popover-navigation']) !!}
+              @endif
+            </div>
           </div>
         </div>
-      </div>
-    @endif
-  </div>
+      @endif
+    </div>
+  @endif
 </div>
 
 <div class="masthead-ad-unit">
