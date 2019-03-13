43a5d767fa8df38fb92e468d92a339f2f1026194
John Puddephatt <john@jdp.org.uk>
2019-03-13 23:32:57 +0000

    revert social share encoding 10

--- resources/inc/newsshooter-socialshare.php
+++ resources/inc/newsshooter-socialshare.php
@@ -9,10 +9,11 @@ add_action('before_content', 'newsshooter_socialshare', 14);
 
 function newsshooter_socialshare() {
 	if( is_single() ) {
-		$sURL = get_permalink();
+        $sURL = get_permalink();
         $title = wp_title('–',false,'right');
+
+        $page_title = html_entity_decode($title, ENT_COMPAT, 'UTF-8');
         $page_title = str_replace('&', '%26', $title);
-		// $page_title = html_entity_decode($title, ENT_COMPAT, 'UTF-8');
 
 		$socialicons = [];
 
