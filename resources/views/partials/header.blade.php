6a2ed388c2ce2cd9e89f615955b2f7b96f792cf5
John Puddephatt <john@jdp.org.uk>
2019-05-14 12:29:32 +0100

    navbar on all pages

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -36,40 +36,38 @@
 </header>
 
 <div class="primary-navigation--wrapper">
-  @if (!is_single())
-    <div class="container">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navigation primary-navigation']) !!}
-        <div class="menu-link--container">
-          <button class="menu-link">
-            <span class="icon">
-              <i class="fa fa-bars"></i>
-            </span>
-            <span class="sr-only menu-link--text">
-              Menu
-            </span>
-          </button>
-          <div class="menu-form--container">
-            <div class="container">
-              <button class="menu-close">
-                ×
-                <span class="sr-only menu-close--text">
-                  Close
-                </span>
-              </button>
-              <img class="popover-logo" src="@asset('images/NS_Logo_Marque.svg')" width="450"/>
-              @if (has_nav_menu('footer_navigation_left'))
-                {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navigation popover-navigation']) !!}
-              @endif
-              @if (has_nav_menu('footer_navigation_left'))
-                {!! wp_nav_menu(['theme_location' => 'footer_navigation_right', 'menu_class' => 'navigtaion popover-navigation']) !!}
-              @endif
-            </div>
+  <div class="container">
+    @if (has_nav_menu('primary_navigation'))
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navigation primary-navigation']) !!}
+      <div class="menu-link--container">
+        <button class="menu-link">
+          <span class="icon">
+            <i class="fa fa-bars"></i>
+          </span>
+          <span class="sr-only menu-link--text">
+            Menu
+          </span>
+        </button>
+        <div class="menu-form--container">
+          <div class="container">
+            <button class="menu-close">
+              ×
+              <span class="sr-only menu-close--text">
+                Close
+              </span>
+            </button>
+            <img class="popover-logo" src="@asset('images/NS_Logo_Marque.svg')" width="450"/>
+            @if (has_nav_menu('footer_navigation_left'))
+              {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navigation popover-navigation']) !!}
+            @endif
+            @if (has_nav_menu('footer_navigation_left'))
+              {!! wp_nav_menu(['theme_location' => 'footer_navigation_right', 'menu_class' => 'navigtaion popover-navigation']) !!}
+            @endif
           </div>
         </div>
-      @endif
-    </div>
-  @endif
+      </div>
+    @endif
+  </div>
 </div>
 
 <div class="masthead-ad-unit">
